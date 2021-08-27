a65ee4ca705dd9571f206c723a0f908156436c4f
Aghata Sophia <aghata@upplify.com>
2021-08-26 21:51:22 -0300

    Update error and log messages

---
+++ src/shared/constants/error-message.ts
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGE = {
+    duplicatedEmail: {
+        errorMessage: 'Email is already being used.',
+        reason: 'Email is already taken by another user.',
+    },
+    unknownError: {
+        errorMessage: 'Something weird happened, check logs for more detail.',
+        reason: 'Unknown internal server error.',
+    },
+};
