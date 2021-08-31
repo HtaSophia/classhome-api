fe433656756a5a6c30ae486f11c66013966c04f7
Thiago Dutra <thiagodutra@gmail.com>
2021-08-31 20:47:54 -0300

    Update roles.guard.ts

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -16,7 +16,6 @@ export class RolesGuard implements CanActivate {
         }
 
         const request = context.switchToHttp().getRequest();
-
         const user = request.user as Account;
 
         return roles.includes(user.role)
