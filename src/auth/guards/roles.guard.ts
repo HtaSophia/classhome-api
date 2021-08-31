63224fa6af192c96f1d9a09913c94b75ba4bf9c8
tdutra <thiagodutra@gmail.com>
2021-08-31 20:46:05 -0300

    Removed password attribute from results, added decorator and RoleGuard

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { Account } from "src/account/account.schema";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+
+    constructor (private reflector: Reflector){}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+        
+        if (!roles) {
+            return true;
+        }
+
+        const request = context.switchToHttp().getRequest();
+
+        const user = request.user as Account;
+
+        return roles.includes(user.role)
+    }
+
+}
--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -1,3 +1,4 @@
+import { RolesGuard } from './../auth/guards/roles.guard';
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ObjectIdPipe } from '../pipes/object-id.pipe';
 import { Account } from '../account/account.schema';
@@ -8,12 +9,15 @@ import { Class } from './class.schema';
 import { ClassService } from './class.service';
 import { ObjectId } from '../shared/types/object-id-helper';
 import { AddStudentDto } from './dtos/add-student.dto';
+import { Roles } from 'src/account/enum/roles.enum';
+import { hasRole } from 'src/auth/decorators/roles.decorator';
 
 @Controller('class')
-@UseGuards(JwtGuard)
+@UseGuards(JwtGuard, RolesGuard)
 export class ClassController {
     constructor(private readonly classService: ClassService) {}
 
+    @hasRole(Roles.PROFESSOR)
     @Post()
     public async create(@Body() dto: ClassDto): Promise<Class> {
         return this.classService.create(dto);
@@ -29,16 +33,19 @@ export class ClassController {
         return this.classService.getById(_id);
     }
 
+    @hasRole(Roles.PROFESSOR)
     @Put(':id')
     public async update(@Param('id', ObjectIdPipe) _id: ObjectId, @Body() dto: Partial<ClassDto>): Promise<Class> {
         return this.classService.update(_id, dto);
     }
 
+    @hasRole(Roles.PROFESSOR)
     @Post(':id/student')
     public async addStudent(@Param('id', ObjectIdPipe) _id: ObjectId, @Body() dto: AddStudentDto): Promise<Class> {
         return this.classService.addStudents(_id, dto.student);
     }
 
+    @hasRole(Roles.PROFESSOR)
     @Delete(':id/student')
     public async removeStudent(
         @Param('id', ObjectIdPipe) _id: ObjectId,
@@ -47,6 +54,7 @@ export class ClassController {
         return this.classService.removeStudent(_id, studentId);
     }
 
+    @hasRole(Roles.PROFESSOR)
     @Delete(':id')
     public async deleteClass(@Param('id', ObjectIdPipe) _id: ObjectId): Promise<HttpStatus> {
         return this.classService.delete(_id);
