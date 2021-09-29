415dfba75ef090dfbc73e44b79d77a2dd1adbde1
tdutra <thiagodutra@gmail.com>
2021-09-28 22:27:09 -0300

    Removed mapped type and UpdateTopicDto

--- src/topic/topic.controller.ts
+++ src/topic/topic.controller.ts
@@ -3,7 +3,6 @@ import { JwtGuard } from './../auth/guards/jwt.guard';
 import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards } from '@nestjs/common';
 import { TopicService } from './topic.service';
 import { CreateTopicDto } from './dto/create-topic.dto';
-import { UpdateTopicDto } from './dto/update-topic.dto';
 import { UseRoles } from 'src/auth/decorators/roles.decorator';
 import { Roles } from 'src/account/enum/roles.enum';
 import { ObjectIdTransform } from 'src/shared/types/object-id-helper';
@@ -33,7 +32,7 @@ export class TopicController {
 
   @UseRoles(Roles.PROFESSOR)
   @Put(':id')
-  public async update(@Param('id') id: string, @Body() updateTopicDto: UpdateTopicDto) {
+  public async update(@Param('id') id: string, @Body() updateTopicDto: Partial<CreateTopicDto>) {
     return this.topicService.update(ObjectIdTransform(id), updateTopicDto);
   }
   
--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ObjectId, ObjectIdTransform } from "src/shared/types/object-id-helper";
 import { CreateTopicDto } from "./dto/create-topic.dto";
-import { UpdateTopicDto } from "./dto/update-topic.dto";
 import { Topic, TopicDocument } from "./schema/topic.schema";
 
 @Injectable()
@@ -22,6 +21,7 @@ export class TopicService {
 
   public async create(createTopicDto: CreateTopicDto): Promise<Topic> {
     this.logger.log(`Creating new topic...`);
+
     return await this.topics.create({ createTopicDto });
   }
 
@@ -31,11 +31,13 @@ export class TopicService {
 
   public async findOne(_id: ObjectId): Promise<Topic> {
     this.logger.log(`Searching for topic with id: ${_id.toString()}`);
+
     return await this.topics.findOne({ _id });
   }
 
-  public async update(_id: ObjectId, updateTopicDto: UpdateTopicDto) {
+  public async update(_id: ObjectId, updateTopicDto: Partial<CreateTopicDto>) {
     this.logger.log(`Updating topic with id: ${_id.toString()}`);
+
     const updatedTopic = await this.topics.findOneAndUpdate(
       { _id },
       { $set: updateTopicDto },
@@ -47,9 +49,13 @@ export class TopicService {
 
   public async remove(_id: ObjectId): Promise<HttpStatus> {
     this.logger.log(`Removing topic with id: ${_id.toString()}`);
+
     const deleted = await this.topics.findOneAndDelete({ _id });
+
     if (deleted) return HttpStatus.OK;
+
     this.logger.log(`Topic with id: ${_id.toString()} was not found`);
+
     return HttpStatus.NOT_FOUND;
   }
 }
